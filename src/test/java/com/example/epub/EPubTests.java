78dbcc03d6c0c3fcc88516a17a7122f2578a3c9e
exquisitesoda <mbilal.5@icloud.com>
2025-07-04 16:25:13 -0400

    move EPubTests setup to constructor

--- src/test/java/com/example/epub/EPubTests.java
+++ src/test/java/com/example/epub/EPubTests.java
@@ -7,13 +7,19 @@ import org.junit.jupiter.api.*;
 
 public class EPubTests
 {
-	@Test
-	public void readTitle() throws Exception
+	EPub epub;
+	
+	public EPubTests() throws Exception
 	{
 		Path path = Paths.get("src/test/resources/progit.epub");
 		ZipFile zipFile = new ZipFile(path.toFile());
-		EPub epub = new EPub(zipFile);
-		
-		Assertions.assertEquals("Pro Git", epub.getTitle());
+		epub = new EPub(zipFile);
+	}
+	
+	@Test
+	public void readTitle()
+	{
+		String title = epub.getTitle();
+		Assertions.assertEquals("Pro Git", title);
 	}
 }
